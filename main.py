183f2d75195de87298f13507bdf4aa7b3711de92
SciDevPablo <140279590+SciDevPablo@users.noreply.github.com>
2023-07-26 15:08:55 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+class Usuario:
+    def __init__(self, cpf, endereco):
+        self.cpf = cpf
+        self.endereco = endereco
+
+
 class ContaBancaria:
-    def __init__(self, saldo_inicial=1000):
+    def __init__(self, usuario, saldo_inicial=1000):
+        self.usuario = usuario
+        self.agencia = "0001"
+        self.numero_conta = len(contas_bancarias) + 1
         self.saldo = saldo_inicial
         self.extrato = []
         self.saques_diarios = 0
@@ -34,17 +43,97 @@ class ContaBancaria:
         print(f"Saldo atual: R${self.saldo:.2f}")
 
 
-# Função principal para executar o sistema
-def main():
-    # Cria uma conta bancária com saldo inicial de 1000
-    conta = ContaBancaria(saldo_inicial=1000)
+class ContaCorrente(ContaBancaria):
+    def __init__(self, usuario, saldo_inicial=1000):
+        super().__init__(usuario, saldo_inicial)
 
+
+class ContaPoupanca(ContaBancaria):
+    def __init__(self, usuario, saldo_inicial=1000):
+        super().__init__(usuario, saldo_inicial)
+        # Aqui você pode adicionar atributos específicos da conta poupança, como taxa de juros, por exemplo.
+
+
+# Lista para armazenar as contas bancárias
+contas_bancarias = []
+
+
+def cadastrar_usuario():
+    cpf = input("Digite o CPF do cliente: ")
+    endereco = input("Digite o endereço do cliente: ")
+    # Verifica se o CPF já está cadastrado
+    if any(conta.usuario.cpf == cpf for conta in contas_bancarias):
+        print("CPF já cadastrado para outro cliente.")
+        return
+    usuario = Usuario(cpf, endereco)
+    opcao_conta = int(input("Escolha o tipo de conta:\n1. Conta Corrente\n2. Conta Poupança\n"))
+    if opcao_conta == 1:
+        conta = ContaCorrente(usuario)
+    elif opcao_conta == 2:
+        conta = ContaPoupanca(usuario)
+    else:
+        print("Opção inválida.")
+        return
+
+    contas_bancarias.append(conta)
+    print(f"Conta criada com sucesso. Número da conta: {conta.numero_conta}")
+
+
+def consultar_saldo():
+    numero_conta = int(input("Digite o número da conta: "))
+    conta = obter_conta_por_numero(numero_conta)
+    if conta:
+        saldo_atual = conta.consultar_saldo()
+        print(f"Saldo atual da conta {numero_conta}: R${saldo_atual:.2f}")
+    else:
+        print("Conta não encontrada.")
+
+
+def realizar_deposito():
+    numero_conta = int(input("Digite o número da conta para o depósito: "))
+    conta = obter_conta_por_numero(numero_conta)
+    if conta:
+        valor_deposito = float(input("Digite o valor a ser depositado: "))
+        conta.depositar(valor_deposito)
+        print("Depósito realizado com sucesso!")
+    else:
+        print("Conta não encontrada.")
+
+
+def realizar_saque():
+    numero_conta = int(input("Digite o número da conta para o saque: "))
+    conta = obter_conta_por_numero(numero_conta)
+    if conta:
+        valor_saque = float(input("Digite o valor a ser sacado: "))
+        conta.sacar(valor_saque)
+    else:
+        print("Conta não encontrada.")
+
+
+def imprimir_extrato():
+    numero_conta = int(input("Digite o número da conta para imprimir o extrato: "))
+    conta = obter_conta_por_numero(numero_conta)
+    if conta:
+        conta.imprimir_extrato()
+    else:
+        print("Conta não encontrada.")
+
+
+def obter_conta_por_numero(numero_conta):
+    for conta in contas_bancarias:
+        if conta.numero_conta == numero_conta:
+            return conta
+    return None
+
+
+def main():
     while True:
         print("\nMenu:")
-        print("1. Consultar Saldo")
-        print("2. Realizar Depósito")
-        print("3. Realizar Saque")
-        print("4. Imprimir Extrato")
+        print("1. Cadastrar Usuário e Conta")
+        print("2. Consultar Saldo")
+        print("3. Realizar Depósito")
+        print("4. Realizar Saque")
+        print("5. Imprimir Extrato")
         print("0. Sair")
 
         opcao = int(input("Digite o número da opção desejada: "))
@@ -53,17 +142,15 @@ def main():
             print("Obrigado por utilizar o sistema bancário. Até mais!")
             break
         elif opcao == 1:
-            saldo_atual = conta.consultar_saldo()
-            print(f"Saldo atual: R${saldo_atual:.2f}")
+            cadastrar_usuario()
         elif opcao == 2:
-            valor_deposito = float(input("Digite o valor a ser depositado: "))
-            conta.depositar(valor_deposito)
-            print("Depósito realizado com sucesso!")
+            consultar_saldo()
         elif opcao == 3:
-            valor_saque = float(input("Digite o valor a ser sacado: "))
-            conta.sacar(valor_saque)
+            realizar_deposito()
         elif opcao == 4:
-            conta.imprimir_extrato()
+            realizar_saque()
+        elif opcao == 5:
+            imprimir_extrato()
         else:
             print("Opção inválida. Tente novamente.")
 
